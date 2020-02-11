db37bf206c0fbdf73e35ce5e66e622ddb8c64881
TomatoSoup <jonathantang0126@gmail.com>
2020-02-11 18:53:29 +0800

    split getPaper function for contextMenus

--- event.js
+++ event.js
@@ -1,5 +1,20 @@
-chrome.browserAction.onClicked.addListener(function (tab) {
-  var newUrl = "https://sci-hub.tw/" + tab.url;
-  chrome.tabs.create({ url: newUrl });
-});
+function getPaper(query) {
+  var newUrl = "https://sci-hub.tw/" + query
+  chrome.tabs.create({ url: newUrl })
+}
 
+
+chrome.browserAction.onClicked.addListener(function (tab) {
+  getPaper(tab.url)
+})
+
+
+chrome.contextMenus.create({
+  type: 'normal',
+  title: 'get by doi: %s',
+  id: 'find sci paper',
+  contexts: ["selection"],
+  onclick: function (info,tab) {
+    getPaper(info.selectionText)
+  }
+})
