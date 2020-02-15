63991f1bc3ba1610eec4164aa4cf56ae3377501d
TomatoSoup <jonathantang0126@gmail.com>
2020-02-15 20:07:22 +0800

    add custom option page and permissions

---
+++ options.js
@@ -0,0 +1,26 @@
+function save_options() {
+  let domain = document.getElementById('domainName').value;
+  
+  chrome.storage.sync.set({
+    coustomDomain: domain,
+  }, function () {
+    let status = document.getElementById('status');
+    status.textContent = 'Options saved.';
+    setTimeout(function () {
+      status.textContent = '';
+    }, 1000);
+  });
+}
+
+
+function restore_options() {
+  chrome.storage.sync.get({
+    coustomDomain: 'tw',
+  }, function (items) {
+      document.getElementById('domainName').value = items.coustomDomain
+  })
+}
+
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click',
+  save_options);
