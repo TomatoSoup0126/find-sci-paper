4fce0286216ce8c2831fcb3f95e74575802535b2
TomatoSoup <jonathantang0126@gmail.com>
2020-09-27 23:29:17 +0800

    change default domain

--- event.js
+++ event.js
@@ -1,14 +1,14 @@
-let domainName = 'tw'
+let domainName = 'se'
 
 function getPaper(query) {
   chrome.storage.sync.get({
-    coustomDomain: 'tw',
+    coustomDomain: 'se',
   }, function (items) {
     if (typeof items.coustomDomain !== 'undefined') {
       domainName = items.coustomDomain
     }
    
-    let newUrl = `https://sci-hub.${domainName}/` + query
+      let newUrl = `https://sci-hub.${domainName}/${query}`
     chrome.tabs.create({ url: newUrl })
   })
 }
